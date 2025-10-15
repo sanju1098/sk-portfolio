936da6517c191cdb613c4bc7e1b13c50d397f4eb
sanju1098 <kumar.sanjay101998@gmail.com>
2025-10-15 11:52:01 +0530

    Updated Work Experiences section

--- src/config/experience.tsx
+++ src/config/experience.tsx
@@ -25,8 +25,9 @@ export const workExperiences = [
       {
         name: "Tech Events Portal",
         responsibilities: [
-          "Architected and delivered a scalable event management platform in Morgan Stanley’s ETS, built with modular ShadCN UI components and a dynamic multi-step form using React Hook Form + Zod for complete validation and seamless UX.",
-          "Optimized frontend performance using TanStack Query with efficient API caching, dynamic calendar/list views, and responsive client-side filters.",
+          "Architected and delivered a scalable event management platform using React.js, TypeScript, and ShadCN UI, featuring modular, accessible components that improved UI consistency, development speed, and maintainability across modules.",
+          "Built a dynamic multi-step event form with React Hook Form and Zod, incorporating 20+ conditional fields and achieving 100% validation coverage, significantly reducing user submission errors and enhancing data accuracy.",
+          "Enhanced application performance and UX by optimizing APIs with TanStack Query for efficient caching and fetch management, implementing role-based access control, and integrating advanced filters, calendar/list views, and CSV exports for event data.",
         ],
       },
     ],
@@ -41,23 +42,26 @@ export const workExperiences = [
       {
         name: "Airline Retailing Platform.",
         responsibilities: [
-          "Developed a real-time airline order creation and tracking UI for Iberia Airline using React.js and TypeScript, integrating the IATA Airline Retailing Order View Schema (v21.3.4) and RESTful APIs within a microservices architecture.",
-          "Enhanced page performance by 25% through route-based lazy loading and dynamic code splitting, while mentoring junior engineers on Webpack and modular UI design, improving sprint delivery efficiency.",
+          "Developed a real-time airline order tracking UI using React.js and TypeScript, visualizing IATA Airline Retailing Order View RS (v21.3.4) data in a structured, user-friendly interface with real-time updates and dynamic order visualization.",
+          "Built a modern, responsive web application featuring route-based lazy loading, dynamic code splitting, and optimized rendering, reducing page load time and improving workflow reliability and user experience.",
+          "Streamlined the order creation process through intuitive progress tracking and optimized UI design, reducing user errors and boosting task completion efficiency across key modules.",
         ],
       },
       {
         name: "AI-Powered Conversational Chat UI (Digital Engineer)",
         responsibilities: [
-          "Built an AI-powered conversational chat interface using Next.js, Vercel, and useChat SDK, handling 1K+ daily messages with source-cited responses.",
-          "Improved API response time by 2s using React Query caching/retries and increased CSAT by 15% through an integrated feedback system.",
-          "Optimized build size and streamlined CI/CD using Docker multi-stage builds, deploying efficiently to Azure with the DevOps team.",
+          "Configured Next.js for development, testing, and production environments, optimizing build processes and streamlining multi-stage Docker deployments and Azure CI/CD pipelines in collaboration with DevOps teams, significantly reducing build time.",
+          "Designed and developed a real-time AI-driven chat interface using Next.js and Vercel’s useChat AI/React SDK, delivering document-based responses and handling 1K+ daily messages with high reliability and performance.Designed and developed a real-time AI-driven chat interface using Next.js and Vercel’s useChat AI/React SDK, delivering document-based responses and handling 1K+ daily messages with high reliability and performance.",
+          "Created an engaging UX/UI in Figma, implementing React Query caching, retries, and feedback logic to enhance responsiveness, reduce load times, and improve AI response accuracy and user experience.",
+          "Implemented dynamic routing and SSO integration via Microsoft Entra, ensuring secure, consistent real-time sessions; collaborated with backend, QA, and DevOps teams to optimize API performance, enhance scalability, and improve CSAT by 15%.",
         ],
       },
       {
         name: "Unisys Component Library (UI UX Framework).",
         responsibilities: [
-          "Delivered a React-based internal component library (npm package/design system) with 75+ WCAG-compliant UI components, showcased in Storybook, reducing UI development time and improving cross-team consistency.",
-          "Acted as Scrum Master and frontend developer, facilitating sprints, resolving blockers, and conducting code reviews — boosting team velocity by 20% and ensuring consistent, high-quality UI delivery.",
+          "Designed, developed, and maintained a scalable React-based Web Component Library with 75+ reusable, WCAG-compliant UI components, improving flexibility, consistency, and development speed across multiple projects.",
+          "Architected and documented UI components in Storybook, integrated global theming for consistent branding, and packaged the library as an NPM module using Rollup, enabling easy distribution, versioning, and reuse across teams.",
+          "Built cross-platform demo applications (Web and Mobile) showcasing component usage, customization, and responsive behavior; implemented unit tests (85–100% coverage) using Jest and React Testing Library to ensure code reliability.",
         ],
       },
     ],
@@ -72,9 +76,10 @@ export const workExperiences = [
       {
         name: " VMware CSP Market Place (Client: VMWare)",
         responsibilities: [
-          "Developed responsive, reusable UI components using React.js, HTML5, CSS3, and VMware Clarity Design System, documented via Storybook for consistency and maintainability.",
-          "Migrated class-based to functional components, added robust form validation and error handling for version control and EULA workflows, improving reliability and accuracy.",
-          "Improved WCAG 2.0 accessibility compliance through NVDA testing and resolution of 100+ ARIA/contrast issues, while actively contributing to Agile sprints and SDLC practices.",
+          "Developed and optimized responsive, reusable UI components using React.js, HTML5, CSS3, and VMware Clarity Design System, ensuring scalability, maintainability, and performance across key modules; documented components via Storybook for consistency and reusability.",
+          "Migrated class components to functional components, implemented robust form validation, error handling, and EULA/version control workflows, enhancing reliability, maintainability, and overall code quality.",
+          "Improved accessibility compliance from 50% to 80% by resolving 100+ ARIA and contrast issues, ensuring adherence to WCAG 2.0 standards and enhancing screen reader usability through NVDA testing.",
+          "Ensured high code quality (85–100% unit test coverage) using React Testing Library and Enzyme, participated in Agile sprint cycles, conducted pull request reviews, and contributed to debugging, performance optimization, and best coding practices.",
         ],
       },
     ],
@@ -89,7 +94,8 @@ export const workExperiences = [
       {
         name: " Custom Local Retail Market Aggregator Platform (Custom LMAP)",
         responsibilities: [
-          "Built end-to-end product and order management features by designing PostgreSQL schemas, developing Django REST API layers, and integrating full-stack UI screens for seamless data interaction",
+          "Collaborated on developing Shop Manager web pages and contributed to the UI team’s technical requirements, executing full-stack development using React, Python, Django, and Django REST Framework to build seamless Product and Order management flows integrated with tested REST APIs.",
+          "Designed and implemented PostgreSQL database schemas for Product, Order, and Stock modules, created corresponding models, serializers, and views, ensuring smooth end-to-end functionality across backend and frontend systems.",
         ],
       },
     ],
